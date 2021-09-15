0affba943f4433fd3675594e647e67dc9c6d9969
Brightson-George <70912692+Brightson-George@users.noreply.github.com>
2021-09-15 18:10:21 -0400

    Add files via upload

---
+++ threadIdx_print.cu
@@ -0,0 +1,25 @@
+﻿#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include <stdio.h>
+
+__global__ void print_threadIDs() {
+	printf("threadIdx_x : %d, threadIdx : %d, threadIdx : %d \n",
+		threadIdx.x, threadIdx.y, threadIdx.z);
+}
+
+int main() {
+	int nx, ny;
+	nx = 16;
+	ny = 16;
+
+	dim3 block(8, 8);
+	dim3 grid(nx / block.x, ny / block.y);
+
+	print_threadIDs << <grid, block >> > ();
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+
+	return 0;
+}
